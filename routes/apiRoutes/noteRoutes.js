cbaa69b5b9c9cd0542278bccc259bba44960e15b
JaimeVilches87 <jaimevilches87@gmail.com>
2021-07-23 03:41:07 -0400

    heroku server

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,20 +1,25 @@
 const router = require("express").Router();
-const {notes} = require('../../db/db');
-const {createNewNote,deleteNote} = require('../../lib/noteFunctions');
+const {
+    notes
+} = require('../../db/db');
+const {
+    createNewNote,
+    deleteNote
+} = require('../../lib/noteFunctions');
 
 
-router.get('/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
     let saved = notes;
     res.json(saved);
 })
 
-router.post('/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     req.body.id = notes.length.toString();
     let note = createNewNote(req.body, notes);
     res.json(note);
 })
 
-router.delete('/notes/:id', (req, res) => {
+router.delete('/api/notes/:id', (req, res) => {
     deleteNote(notes, req.params.id);
     res.json(notes);
 })
