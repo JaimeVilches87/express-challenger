b6bfdd06d6657d2af440200bf8a4cbde359d8d78
JaimeVilches87 <jaimevilches87@gmail.com>
2021-07-23 04:11:53 -0400

    added htlm routes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -8,18 +8,18 @@ const {
 } = require('../../lib/noteFunctions');
 
 
-router.get('/api/notes', (req, res) => {
+router.get('api/notes', (req, res) => {
     let saved = notes;
     res.json(saved);
 })
 
-router.post('/api/notes', (req, res) => {
+router.post('api/notes', (req, res) => {
     req.body.id = notes.length.toString();
     let note = createNewNote(req.body, notes);
     res.json(note);
 })
 
-router.delete('/api/notes/:id', (req, res) => {
+router.delete('api/notes/:id', (req, res) => {
     deleteNote(notes, req.params.id);
     res.json(notes);
 })
