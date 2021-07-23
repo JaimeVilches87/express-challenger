8aebbc3a153469076e5cb38b9585d4cbe8ecd4cf
JaimeVilches87 <jaimevilches87@gmail.com>
2021-07-23 02:24:24 -0400

    added server.js

--- server.js
+++ server.js
@@ -1,31 +1,21 @@
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-
+const PORT = process.env.PORT || 3001;
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes.js');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
+app.use(express.urlencoded({
+    extended: true
+}));
 
-// Tells node that we are creating an "express" server
-const app = express();
-
-// Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT || 3001;
-
-// Sets up the Express app to handle data parsing
-
+app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('./Develop/public'));
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
+});
