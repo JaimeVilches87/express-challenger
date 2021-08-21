e0ff6824a16e4bbbf2b382a4643a99e251c217d3
JaimeVilches87 <jaimevilches87@gmail.com>
2021-08-20 21:01:14 -0400

    added functions to lib

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
-const {
-    notes
+const { notes
 } = require('../../db/db');
 const {
     createNewNote,
@@ -8,21 +7,21 @@ const {
 } = require('../../lib/noteFunctions');
 
 
-router.get('api/notes', (req, res) => {
-    let saved = notes;
-    res.json(saved);
-})
+router.get('/notes', (req, res) => {
 
-router.post('api/notes', (req, res) => {
+    getNotes()
+        .then((notes) => {
+            return res.json(notes);
+        })
+        .catch((err) => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
     req.body.id = notes.length.toString();
     let note = createNewNote(req.body, notes);
     res.json(note);
 })
 
-router.delete('api/notes/:id', (req, res) => {
-    deleteNote(notes, req.params.id);
-    res.json(notes);
-})
 
 
 module.exports = router;
